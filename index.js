935a8faf6bbcf506650abab94d0107fd4d551156
DILIP KUMAR JAKHAR <jakhardilip10@gmail.com>
2022-05-25 12:19:55 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,23 @@
+let typed = new Typed(".auto-input", {
+    strings: ["Dilip Kumar","Dilip Kumar Jakhar", "Dilip Kumar Jakhar"],
+    typeSpeed: 180,
+    backSpeed: 180,
+    loop: true,
+    
+  });
+
+
+
+
+
+
+
+  
+
+  function dilip(){
+   window.location.href ="home.html";
+       
+ }
+           
+                
+
